f96d664e8075e2536761d469881b41bda709437e
Chad Hantak <hantak@gmail.com>
2019-11-19 10:10:52 -0700

    Added documentation to matcher process Moved matcher.cs to proper namespace folder

--- vivo.matcher/vivo/Matcher.cs
+++ vivo.matcher/vivo/Matcher.cs
@@ -23,6 +23,17 @@ namespace vivo
 
 
 
+		// Process the triples created by the harvester
+		// For each author, see if there is a match in our faculty
+		// If there is, rewrite their triples so instead of making a new person, 
+		//   the author is linked to the existing faculty
+		// If there isn't a match, leave the author's triples
+		// Finally, convert all the triples to the current format VIVO requires (ISF).  The harvester does not generate
+		// the correct triples for VIVO 1.9.  For reference:
+		//   https://wiki.lyrasis.org/display/VIVODOC19x/Publication+Model
+		//   https://wiki.lyrasis.org/display/VIVODOC19x/Person+Model
+		//   https://wiki.lyrasis.org/display/VTDA/VIVO-ISF+Ontology
+
 		public void Process()
 		{
 			// Go through the harvest
@@ -40,8 +51,11 @@ namespace vivo
 						continue;
 					}
 
+					// At this point, we have a potential author to match
+					// Look through the profiles (from vivo-pump) to see if there is a match
 					var match = Profiles.FindMatch(authorship.Author.Name);
 
+					// no match, skip, this means that autor
 					if (match == null) {
 						Console.WriteLine(@"Skipping author " + authorship.Author.Name.Last);
 						continue;
@@ -49,18 +63,23 @@ namespace vivo
 
 					Console.WriteLine(@"Updating " + authorship.Author.LastName + @" to " + match.TargetProfile.Uri);
 
+					// Generates all the triples to remove (this is the new "person" it wants to create for the author)
 					var triplesToRemove = new List<Triple>(authorship.Author.GetTriples());
 					triplesToRemove.Add(authorship.LinkedAuthorTriple);
 
+					// Adds the triple to link the author
 					authorship.UpdateLinkedAuthor(match.TargetProfile.Uri);
 
+					// Remove all the triples
 					IncomingHarvest.Remove(triplesToRemove);
 				}
 			}
 
-
+			// Convert triples to the ISF format
 			IncomingHarvest.UpdateToISF();
 
+			// Export the triples (currently this just pushes RDF)
+			// TODO: Check the filename and output the correct format (RDF vs n3 triples)
 			IncomingHarvest.ExportRdf(OutputHarvestFilename);
 		}
 
@@ -99,10 +118,12 @@ namespace vivo
 			// Load the RDF from the harvester
 			var harvest = vivo.rdf.harvest.Pubmed.LoadFromXML(incomingFilename);
 
+			// Load the faculty information from CSV file (comes from the vivo pump)
 			var facultyProfiles = profiles.ProfileList.Load(profilesFilename);
 
 			var m = new Matcher(facultyProfiles, harvest, outputFilename);
 
+			// Run the matcher (will output to outputFilename)
 			m.Process();
 
 		}
